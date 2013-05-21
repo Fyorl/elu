1a9f1e10d3ae24107db48c567e3ba5c1880c31ea
Fyorl <gaelreth@gmail.com>
2013-05-21 01:14:32 +0100

    Added basic unit testing framework and some initial tests.

---
+++ tests/run.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "minunit.h"
+
+#include "test_string_utils.c"
+
+int tests_run = 0;
+
+static char* all_tests () {
+	mu_run_test(test_string_utils);
+	return 0;
+}
+
+int main (int argc, char** argv) {
+	char* result = all_tests();
+	if (result != 0) {
+		printf("FAIL\n%s\n", result);
+	} else {
+		printf("PASS\n");
+	}
+
+	printf("Ran %d tests.\n", tests_run);
+	return (result != 0);
+}
---
+++ tests/test_string_utils.c
@@ -0,0 +1,31 @@
+#include <string.h>
+
+#include "minunit.h"
+#include "../string_utils.c"
+
+char tsu__str1[4];
+char tsu__str2[3];
+
+void tsu__callback (const char* string, void* data) {
+	static int call_count = 0;
+	if (call_count == 0) {
+		strcpy(tsu__str1, string);
+	} else if (call_count == 1) {
+		strcpy(tsu__str2, string);
+	}
+
+	call_count++;
+}
+
+static char* test_string_split_cb () {
+	string_split_cb(&tsu__callback, NULL, "abcdef", 'd');
+	mu_assert("split[0] != 'abc'", strcmp("abc", tsu__str1) == 0);
+	mu_assert("split[1] != 'ef'", strcmp("ef", tsu__str2) == 0);
+
+	return 0;
+}
+
+static char* test_string_utils () {
+	mu_run_test(test_string_split_cb);
+	return 0;
+}
