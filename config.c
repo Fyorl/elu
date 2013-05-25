1db31303172098f21718fe17d5086cd382163d4a
Fyorl <gaelreth@gmail.com>
2013-05-25 19:04:52 +0100

    Added comments to config file.

--- config.c
+++ config.c
@@ -15,10 +15,13 @@ void parse_lines (const char* line, void* data) {
 	vector_init(&vector, sizeof(char*));
 	string_split(&vector, line, '=');
 
+	// We split each line of the config file by the '='. The first element is
+	// the key and the second is the value.
 	char* key = vector_get(vector, 0, char*);
 	char* value = vector_get(vector, 1, char*);
 	char** field = NULL;
 
+	// Check for those keys which we only need to store directly as strings.
 	if (strcmp("host", key) == 0) {
 		field = &(global_config->host);
 	} else if (strcmp("nick", key) == 0) {
@@ -27,14 +30,21 @@ void parse_lines (const char* line, void* data) {
 		field = &(global_config->pass);
 	}
 
+	// If field is null then our key is not one we can just store as a straight
+	// string and must instead do some conversion on it.
 	if (field == NULL) {
 		if (strcmp("port", key) == 0) {
+			// The port must be converted into a 16-bit int.
 			global_config->port = (uint16_t) strtol(value, NULL, 10);
 		} else if (strcmp("channels", key) == 0) {
+			// The channels we can just store as a vector by splitting on the
+			// comma.
 			vector_init(&(global_config->channels), sizeof(char*));
 			string_split(&(global_config->channels), value, ',');
 		}
 	} else {
+		// If we can ust store it as a string then allocate some permanent
+		// memory and do just that.
 		*field = calloc(strlen(value) + 1, sizeof(char));
 		strcpy(*field, value);
 	}
