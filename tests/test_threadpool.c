63db85d305d6d1ce6c292825a3ae45f3e6fd9e85
Fyorl <gaelreth@gmail.com>
2013-06-01 23:53:38 +0100

    Added test for single threaded threadpool.

--- tests/test_threadpool.c
+++ tests/test_threadpool.c
@@ -3,6 +3,7 @@
 
 pthread_mutex_t tt__count_mutex = PTHREAD_MUTEX_INITIALIZER;
 int tt__worker_count = 0;
+int tt__work_in_order = 1;
 
 void tt__dummy (void* data) {}
 
@@ -13,6 +14,15 @@ void tt__worker (void* data) {
 		tt__worker_count++;
 		pthread_mutex_unlock(&tt__count_mutex);
 	}
+
+	free(data);
+}
+
+void tt__ordered_worker (void* data) {
+	static int previous = -1;
+	int work = *((int*) data);
+
+	tt__work_in_order = tt__work_in_order && (work == previous + 1);
 
 	free(data);
 }
@@ -52,8 +62,26 @@ static char* test_threadpool_add_work () {
 	return 0;
 }
 
+static char* test_threadpool_single () {
+	queue_t queue;
+	queue_init(&queue, sizeof(int), 0);
+
+	threadpool_t threadpool;
+	threadpool_init(&threadpool, 1, &queue, &tt__ordered_worker);
+
+	int i;
+	for (i = 0; i < 10; i++) {
+		threadpool_add_work(&threadpool, &i);
+	}
+
+	mu_assert("the work was not received in order", tt__work_in_order);
+
+	return 0;
+}
+
 static char* test_threadpool () {
 	mu_run_test(test_threadpool_destroy);
 	mu_run_test(test_threadpool_add_work);
+	mu_run_test(test_threadpool_single);
 	return 0;
 }
