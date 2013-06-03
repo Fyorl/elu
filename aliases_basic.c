620610aeeb7cd478d622472e124ee4110b8f9b8f
Fyorl <gaelreth@gmail.com>
2013-06-03 21:10:41 +0100

    Aliases now work in PMs.

--- aliases_basic.c
+++ aliases_basic.c
@@ -9,9 +9,16 @@ extern int sock;
 
 char* alias_say (const char* nick, const char* channel, const char* msg) {
 	const char* arg = irc_get_arg(msg);
+	const char* output;
 
 	if (arg[0] != '\0') {
-		irc_send_privmsg(channel, arg);
+		if (channel[0] == '#') {
+			output = channel;
+		} else {
+			output = nick;
+		}
+
+		irc_send_privmsg(output, arg);
 	}
 
 	return 0;
@@ -20,11 +27,18 @@ char* alias_say (const char* nick, const char* channel, const char* msg) {
 char* alias_reply (const char* nick, const char* channel, const char* msg) {
 	const char* arg = irc_get_arg(msg);
 	char* reply;
+	const char* output;
 
 	if (arg[0] != '\0') {
+		if (channel[0] == '#') {
+			output = channel;
+		} else {
+			output = nick;
+		}
+
 		reply = calloc(strlen(nick) + strlen(arg) + 3, sizeof(char));
 		sprintf(reply, "%s: %s", nick, arg);
-		irc_send_privmsg(channel, reply);
+		irc_send_privmsg(output, reply);
 
 		free(reply);
 	}
