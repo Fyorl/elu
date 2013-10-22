c6df27596e8b540bce8f5003017aa77ddf1c5fea
Fyorl <kim.mantas@gmail.com>
2013-10-22 14:02:59 +0100

    Import db_mutex into timers.

--- timers.c
+++ timers.c
@@ -1,9 +1,12 @@
+#include <pthread.h>
+
 #include "elu.h"
 #include "irc.h"
 #include "timers.h"
 #include "vendor/sqlite3.h"
 
 extern sqlite3* db;
+extern pthread_mutex_t db_mutex;
 
 void check_timers () {
 	
